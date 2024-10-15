23d17375c75fd7bb70fc662bac70c66fb9b3c532
Christer Dale Montilla Reyes <rchristerdale@gmail.com>
2024-10-16 04:19:40 +0800

    Transaction is now working

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:reown_appkit/reown_appkit.dart';
-import 'package:url_launcher/url_launcher.dart'; // Required for launching external apps
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -15,9 +17,43 @@ class _MyHomePageState extends State<MyHomePage> {
   String _balance = '0';
   bool isLoading = false;
 
+  // Define the custom network for Sepolia
+  final customNetwork = ReownAppKitModalNetworkInfo(
+    name: 'Sepolia',
+    chainId: '11155111', // Sepolia chain ID
+    currency: 'ETH',
+    rpcUrl: 'https://rpc.sepolia.org/', // Sepolia RPC URL
+    explorerUrl: 'https://sepolia.etherscan.io/', // Explorer URL for Sepolia
+    isTestNetwork: true, // Set to true for test networks
+  );
+
+  final Set<String> featuredWalletIds = {
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase Wallet
+    'cbc11415130d01316513f735eac34fd1ad7a5d40a993bbb6772d2c02eeef3df8',
+    '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
+  };
+
   @override
   void initState() {
     super.initState();
+    ReownAppKitModalNetworks.addNetworks('eip155', [customNetwork]);
+    ReownAppKitModalNetworks.removeNetworks('eip155', [
+      '10',
+      '100',
+      '137',
+      '324',
+      '1101',
+      '5000',
+      '8217',
+      '42161',
+      '42220',
+      '43114',
+      '59144',
+      '7777777',
+      '1313161554'
+    ]);
     initializeAppKitModal();
   }
 
@@ -37,6 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
           linkMode: true,
         ),
       ),
+      featuredWalletIds: featuredWalletIds,
     );
 
     try {
@@ -82,27 +119,149 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // Method to open the selected wallet app (e.g., MetaMask)
   void openWalletApp() {
-    final selectedWallet = _appKitModal?.selectedWallet?.listing.name;
-
-    if (selectedWallet != null) {
-      // Print the name of the selected wallet
-      debugPrint('Selected wallet: $selectedWallet');
-
-      if (selectedWallet.toLowerCase().contains('metamask')) {
-        final Uri metamaskUri = Uri.parse("metamask://");
-        launchUrl(metamaskUri, mode: LaunchMode.externalApplication);
-      } else {
-        // Handle other wallets if needed
-        debugPrint('No wallet app to open or unsupported wallet.');
+    if (_appKitModal != null && _appKitModal!.isConnected) {
+      try {
+        // Launch the connected wallet using the launchConnectedWallet method
+        _appKitModal!.launchConnectedWallet();
+        debugPrint('Launching connected wallet...');
+      } catch (e) {
+        debugPrint('Error launching connected wallet: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Unable to launch wallet: $e')),
+        );
       }
     } else {
-      debugPrint('No wallet selected.');
+      debugPrint('No wallet connected or session is invalid.');
     }
   }
 
-// Widget for displaying a loading indicator
+  Future<void> testTransaction(String receiver, String amount) async {
+    if (receiver.isEmpty || amount.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Invalid receiver address!')),
+      );
+      return; // Exit the function if the address is invalid
+    }
+    _appKitModal!.launchConnectedWallet();
+    debugPrint(
+        'Debug Transaction: Testing transaction with receiver: $receiver and amount: $amount');
+
+    // Convert amount to Ether
+    double amountInEther = double.parse(amount);
+    debugPrint('Debug Transaction: Amount in Ether: $amountInEther');
+
+    // Convert Ether amount to Wei
+    BigInt amountInWei = BigInt.from((amountInEther * pow(10, 18)).toInt());
+    EtherAmount txValue =
+        EtherAmount.fromUnitAndValue(EtherUnit.wei, amountInWei);
+    debugPrint('Debug Transaction: Amount in Wei: $amountInWei');
+
+    BigInt balanceInWeiValue;
+
+    try {
+      // Get sender address and current balance
+      final senderAddress = _appKitModal!.session!.address!;
+      debugPrint('Debug Transaction: Sender Address: $senderAddress');
+
+      final currentBalance = _appKitModal!.balanceNotifier.value;
+      debugPrint('Debug Transaction: Current Balance: $currentBalance');
+
+      // Check if balance is empty
+      if (currentBalance.isEmpty) {
+        throw Exception('Unable to fetch wallet balance.');
+      }
+
+      // Convert current balance to Ether and then to Wei
+      double balanceInEther = double.parse(currentBalance.split(' ')[0]);
+      balanceInWeiValue = BigInt.from((balanceInEther * pow(10, 18)).toInt());
+      debugPrint('Debug Transaction: Balance in Ether: $balanceInEther');
+    } catch (e) {
+      debugPrint('Debug Transaction: Error parsing wallet balance: $e');
+      throw Exception('Error parsing wallet balance: $e');
+    }
+
+    // Create EtherAmount for current balance in Wei
+    final balanceInWei =
+        EtherAmount.fromUnitAndValue(EtherUnit.wei, balanceInWeiValue);
+    debugPrint('Debug Transaction: Balance in Wei: $balanceInWei');
+
+    // Calculate total cost including transaction fee
+    final gasPrice = BigInt.from(1000000000); // 1 Gwei
+    final gasLimit = BigInt.from(200000); // Standard gas limit for ETH transfer
+    final totalCost =
+        txValue.getInWei + (gasPrice * gasLimit); // Include gas fees
+    debugPrint(
+        'Debug Transaction: Total Cost (including gas fees): $totalCost');
+
+    // Check if the balance is sufficient for the transaction
+    if (balanceInWei.getInWei < totalCost) {
+      throw Exception(
+          'Insufficient funds for transaction! Balance: ${balanceInWei.getInWei}, Total Cost: $totalCost');
+    }
+
+    // Define the contract and its ABI
+    final tetherContract = DeployedContract(
+      ContractAbi.fromJson(
+        jsonEncode([
+          {
+            "constant": false,
+            "inputs": [
+              {"internalType": "address", "name": "_to", "type": "address"},
+              {"internalType": "uint256", "name": "_value", "type": "uint256"}
+            ],
+            "name": "transfer",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+          }
+        ]),
+        'ETH',
+      ),
+      EthereumAddress.fromHex(receiver),
+    );
+
+    // Send the transaction
+    try {
+      final result = await _appKitModal!.requestWriteContract(
+        topic: _appKitModal!.session!.topic,
+        chainId: _appKitModal!.selectedChain!.chainId,
+        deployedContract: tetherContract,
+        functionName: 'transfer',
+        transaction: Transaction(
+          from: EthereumAddress.fromHex(_appKitModal!.session!.address!),
+          to: EthereumAddress.fromHex(receiver),
+          value: txValue,
+          maxGas: gasLimit.toInt(),
+        ),
+        parameters: [
+          EthereumAddress.fromHex(receiver),
+          amountInWei, // Amount in Wei
+        ],
+      );
+      debugPrint('Debug Transaction: Transaction request result: $result');
+      // _appKitModal!.launchConnectedWallet();
+      // Handle result
+      if (result != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Transaction successful!')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Transaction failed.')),
+        );
+      }
+    } catch (e) {
+      // Enhanced error handling
+      debugPrint('Debug Transaction: Transaction error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Transaction error: $e')),
+      );
+    }
+  }
+
+  // Widget for displaying a loading indicator
   Widget loadingIndicator() {
     return isLoading
         ? const CircularProgressIndicator()
@@ -112,65 +271,142 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     debugPrint('AppKitModal instance: $_appKitModal');
+    final TextEditingController addressController = TextEditingController();
+    final TextEditingController amountController = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Crypto Flutter App'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Button to select a network
-            if (_appKitModal != null)
-              AppKitModalNetworkSelectButton(appKit: _appKitModal!),
+        child: SingleChildScrollView(
+          // Allow scrolling if content overflows
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Button to select a network
+              if (_appKitModal != null)
+                AppKitModalNetworkSelectButton(appKit: _appKitModal!),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // Button to connect the wallet
-            if (_appKitModal != null)
-              Visibility(
-                visible: _appKitModal?.isConnected ?? false,
-                child: AppKitModalConnectButton(appKit: _appKitModal!),
+              // Button to connect the wallet
+              if (_appKitModal != null)
+                Visibility(
+                  visible: _appKitModal?.isConnected ?? false,
+                  child: AppKitModalConnectButton(appKit: _appKitModal!),
+                ),
+
+              const SizedBox(height: 20),
+
+              // Display the connected wallet address
+              Text('Connected Wallet: $walletAddress'),
+              const SizedBox(height: 10),
+
+              // Display the wallet balance
+              Text('Balance: $_balance'),
+              const SizedBox(height: 20),
+
+              // Button to launch the wallet app
+              ElevatedButton(
+                onPressed: openWalletApp,
+                child: const Text('Open Wallet App'),
               ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // Only show the account button if the user is connected
-            Visibility(
-              visible: _appKitModal?.isConnected ?? false,
-              child: AppKitModalAccountButton(appKit: _appKitModal!),
-            ),
+              // Grouped TextFields and Sign Message Button
+              if (_appKitModal != null)
+                Visibility(
+                  visible: _appKitModal?.isConnected ?? false,
+                  child: Column(
+                    children: [
+                      // Receiver Address TextField
+                      Container(
+                        width: 250, // Set the width to make it smaller
+                        child: TextField(
+                          controller: addressController,
+                          decoration: InputDecoration(
+                            labelText: 'Receiver Address',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(color: Colors.blue),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(color: Colors.blue),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 20.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
 
-            const SizedBox(height: 20),
+                      // Amount TextField
+                      Container(
+                        width: 250, // Set the width to make it smaller
+                        child: TextField(
+                          controller: amountController,
+                          decoration: InputDecoration(
+                            labelText: 'Amount',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(color: Colors.blue),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(color: Colors.blue),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 12.0, horizontal: 20.0),
+                          ),
+                          keyboardType: TextInputType.number,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
 
-            // Show loading indicator while waiting for wallet connection
-            loadingIndicator(),
+                      ElevatedButton(
+                        onPressed: () {
+                          String recipient = addressController.text;
+                          String amount = amountController.text;
+                          testTransaction(recipient, amount);
+                        },
+                        child: const Text('Test Transaction'),
+                      ),
+                    ],
+                  ),
+                ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // Button to open the wallet app (e.g., MetaMask)
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  isLoading = true;
-                });
-                openWalletApp();
-                setState(() {
-                  isLoading = false;
-                });
-              },
-              child: const Text('Open Wallet App'),
-            ),
-          ],
+              loadingIndicator(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-void main() {
-  runApp(const MaterialApp(
-    home: MyHomePage(),
-  ));
-}
+// final tetherContract = DeployedContract(
+//       ContractAbi.fromJson(
+//         jsonEncode([
+//           {
+//             "constant": false,
+//             "inputs": [
+//               {"internalType": "address", "name": "_to", "type": "address"},
+//               {"internalType": "uint256", "name": "_value", "type": "uint256"}
+//             ],
+//             "name": "transfer",
+//             "outputs": [],
+//             "payable": false,
+//             "stateMutability": "nonpayable",
+//             "type": "function"
+//           }
+//         ]), // ABI object
+//         'ETH', // Name of the contract for clarity
+//       ),
+//       EthereumAddress.fromHex(
+//           receiver), // Tether contract address
+//     );
